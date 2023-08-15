3138ff6b49d76ebd25afd6685b8ed2343c5d7f4c
Rivaldi <rivaldi.irawan@dhealth.co.id>
2023-08-15 15:11:31 +0700

    [IMPROVEMENT] - add librari ripple and fetch api users

---
+++ app/detail/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useState, useEffect } from 'react'
+
+export default function Detail() {
+  const [data, setData] = useState<any>(null)
+  const [isLoading, setLoading] = useState(true)
+ 
+  useEffect(() => {
+    fetch('http://localhost:8080/api/home')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+ 
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No profile data</p>
+  return (
+    <>
+      <div>
+        <h1>{data.message}</h1>
+      </div>
+    </>
+  )
+}
